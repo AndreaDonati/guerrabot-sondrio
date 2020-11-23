27eb41803979de103e6323fc7c95a363dc537b63
AndreaDonati <andrewgifted@users.noreply.github.com>
2020-11-23 13:09:54 +0100

    Update settings.py

--- guerrabot_website/settings.py
+++ guerrabot_website/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# keep it empty for the moment
-STATICFILES_DIRS = (
-)
+# Possible solution to collectstatic problem
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# # keep it empty for the moment
+# STATICFILES_DIRS = (
+# )
